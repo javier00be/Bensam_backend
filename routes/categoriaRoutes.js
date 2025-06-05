ad20fc4d8da70e9c5a221607714b77bd173afb1b
javier00be <javier345989@gmail.com>
2025-06-04 22:12:51 -0500

    creacion de categoria

--- routes/categoriaRoutes.js
+++ routes/categoriaRoutes.js
@@ -3,10 +3,10 @@ const categoriaController = require('../controllers/categoriaController');
 
 const router = express.Router();
 
-router.post('/', categoriaController.crearCategoria);
-router.get('/', categoriaController.obtenerCategorias);
-router.get('/:id', categoriaController.obtenerCategoriaPorId);
-router.put('/:id', categoriaController.actualizarCategoria);
-router.delete('/:id', categoriaController.eliminarCategoria);
+router.post('/crear', categoriaController.crearCategoria);
+router.get('/listar', categoriaController.obtenerCategorias);
+router.get('/listar/:id', categoriaController.obtenerCategoriaPorId);
+router.put('/actualizar/:id', categoriaController.actualizarCategoria);
+router.delete('/eliminar/:id', categoriaController.eliminarCategoria);
 
 module.exports = router;
